05160f48287e3167c3fe47b0ce0796888b5019dc
Adishwar Rishi <adiswa123@gmail.com>
2023-06-04 03:53:22 +1000

    Init with wrangler 2

---
+++ src/worker.ts
@@ -0,0 +1,27 @@
+import { PartialTraceConfig, instrument } from '@microlabs/otel-cf-workers';
+
+export interface Env {}
+
+const handler = {
+  async fetch(request: Request, env: Env, ctx: ExecutionContext): Promise<Response> {
+    return new Response(JSON.stringify({ hello: 'world' }), {
+      status: 200,
+      headers: {
+        'content-type': 'application/json',
+      },
+    });
+  },
+};
+
+const config: PartialTraceConfig = {
+  exporter: {
+    url: 'https://api.honeycomb.io',
+    headers: {
+      // use the simple way for testing
+      'x-honeycomb-team': 'SECRET',
+    },
+  },
+  service: { name: 'worker-greetings' },
+};
+
+export default instrument(handler, config);
